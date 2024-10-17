efd3a57209cf18676526477db180ba7e29fb75c6
Mariia Piterova <mariia999@bk.ru>
2024-10-17 17:36:07 +0300

    Change in scripts

--- Assets/Scripts/Game/Bullet.cs
+++ Assets/Scripts/Game/Bullet.cs
@@ -1,9 +1,26 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace TDS.Game
 {
     public class Bullet :  MonoBehaviour
     {
+        [SerializeField] private Rigidbody2D _rb;
+        [SerializeField] private float _speed = 10f;
+        [SerializeField] private float _lifetime = 3f;
+        
 
+        private void Start()
+        {
+            _rb.velocity = transform.up * _speed;
+            StartCoroutine(DestroyWithLifetimeDelay());
+        }
+
+        private IEnumerator DestroyWithLifetimeDelay()
+        {
+            yield return new WaitForSeconds(_lifetime);
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Game/PlayerAnimation.cs
+++ Assets/Scripts/Game/PlayerAnimation.cs
@@ -1,7 +1,29 @@
+using UnityEngine;
+
 namespace TDS.Game
 {
-    public class PlayerAnimation
+    public class PlayerAnimation : MonoBehaviour
     {
-        
+        #region Variables
+
+        [SerializeField] private Animator _animator;
+        private static readonly int Movement = Animator.StringToHash("movement");
+        private static readonly int Fire = Animator.StringToHash("fire");
+
+        #endregion
+
+        #region Public methods
+
+        public void SetMovement(float sped)
+        {
+            _animator.SetFloat(Movement, sped);
+        }
+
+        public void TriggerAttack()
+        {
+            _animator.SetTrigger(Fire);
+        }
+
+        #endregion
     }
 }
