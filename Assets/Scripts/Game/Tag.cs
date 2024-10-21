068d2ec41d78f37399b5fe42aef30ddafbc53800
mari999p <mariia999@bk.ru>
2024-10-21 13:49:37 +0300

    Add scripts

---
+++ Assets/Scripts/Game/Tag.cs
@@ -0,0 +1,12 @@
+namespace TDS.Game
+{
+    public static class Tag
+    {
+        #region Variables
+
+        public const string Player = "Player";
+        public const string Enemy = "Enemy";
+
+        #endregion
+    }
+}
