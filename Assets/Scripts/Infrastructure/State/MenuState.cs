5a5712bee7a91fadbcedb45b7e10eacd5aa3d657
mari <mariia999@bk.ru>
2024-11-02 23:50:01 +0300

    Add scripts (MenuState)

---
+++ Assets/Scripts/Infrastructure/State/MenuState.cs
@@ -0,0 +1,13 @@
+namespace TDS.Infrastructure.State
+{
+    public class MenuState : AppState
+    {
+        #region Public methods
+
+        public override void Enter() { }
+
+        public override void Exit() { }
+
+        #endregion
+    }
+}
