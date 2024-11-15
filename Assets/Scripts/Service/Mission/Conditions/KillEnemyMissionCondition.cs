08db79fb5405fd749e70e10c7a726fc4b04a9402
mari <mariia999@bk.ru>
2024-11-15 21:59:20 +0300

    Change in database Mission

--- Assets/Scripts/Service/Mission/Conditions/KillEnemyMissionCondition.cs
+++ Assets/Scripts/Service/Mission/Conditions/KillEnemyMissionCondition.cs
@@ -1,4 +1,4 @@
-using TDS.Game.Common;
+using TDS.Game.Enemy.Base;
 using UnityEngine;
 
 namespace TDS.Service.Mission.Conditions
@@ -7,7 +7,13 @@ namespace TDS.Service.Mission.Conditions
     {
         #region Variables
 
-        [SerializeField] private UnitHp _enemyUnitHp;
+        [SerializeField] private EnemyDeath _enemyDeath;
+
+        #endregion
+
+        #region Properties
+
+        public EnemyDeath EnemyDeath => _enemyDeath;
 
         #endregion
     }
