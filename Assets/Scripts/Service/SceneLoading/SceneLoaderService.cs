9ae7601ee5f7afa3452a42e3252611b5d3357513
mari <mariia999@bk.ru>
2024-11-15 22:04:46 +0300

    Change in script SceneLoaderService

--- Assets/Scripts/Service/SceneLoading/SceneLoaderService.cs
+++ Assets/Scripts/Service/SceneLoading/SceneLoaderService.cs
@@ -1,4 +1,7 @@
+using System.Collections;
 using TDS.Infrastructure.Locator;
+using TDS.Infrastructure.State;
+using TDS.Service.Coroutine;
 using UnityEngine.SceneManagement;
 
 namespace TDS.Service.SceneLoading
@@ -13,6 +16,22 @@ namespace TDS.Service.SceneLoading
         }
 
         public void LoadAsync(string sceneName) { }
+
+        public void RetrySceneWithDelay(string sceneName)
+        {
+            ServicesLocator.Instance.Get<CoroutineRunner>().StartCoroutine(RetryLevelWithDelay(sceneName));
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private IEnumerator RetryLevelWithDelay(string sceneName)
+        {
+            yield return null;
+            Load(sceneName);
+            ServicesLocator.Instance.Get<StateMachine>().Enter<GameState>();
+        }
 
         #endregion
     }
