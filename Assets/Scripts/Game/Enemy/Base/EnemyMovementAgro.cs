2dd9218df786acf2e0b09c1b2bdcbc969776f747
mari <mariia999@bk.ru>
2024-11-18 17:20:07 +0300

    3 task

--- Assets/Scripts/Game/Enemy/Base/EnemyMovementAgro.cs
+++ Assets/Scripts/Game/Enemy/Base/EnemyMovementAgro.cs
@@ -14,6 +14,7 @@ namespace TDS.Game.Enemy.Base
         [SerializeField] private LayerMask _obstacleMask;
 
         [SerializeField] [Range(0, 360)] private float _viewAngle = 90f;
+        [SerializeField] [Range(0, 360)] private float _initialDirectionAngle;
         [SerializeField] private float _viewDistance = 10f;
         [SerializeField] private float _noticeDistance = 3f;
 
@@ -54,8 +55,11 @@ namespace TDS.Game.Enemy.Base
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.magenta;
-            Vector3 leftBoundary = Quaternion.Euler(0, 0, -_viewAngle / 2) * transform.right;
-            Vector3 rightBoundary = Quaternion.Euler(0, 0, _viewAngle / 2) * transform.right;
+            Vector3 initialDirection = Quaternion.Euler(0, 0, _initialDirectionAngle) * transform.right;
+
+            Vector3 leftBoundary = Quaternion.Euler(0, 0, -_viewAngle / 2) * initialDirection;
+            Vector3 rightBoundary = Quaternion.Euler(0, 0, _viewAngle / 2) * initialDirection;
+
             Gizmos.DrawLine(transform.position, transform.position + leftBoundary * _viewDistance);
             Gizmos.DrawLine(transform.position, transform.position + rightBoundary * _viewDistance);
             Gizmos.DrawWireSphere(transform.position, _viewDistance);
@@ -80,7 +84,7 @@ namespace TDS.Game.Enemy.Base
             _idle.Activate();
         }
 
-        private void TriggerStayedCallback(Collider2D col)
+        private void TriggerStayedCallback(Collider2D col) //3
         {
             if (_isFollowing || !col.CompareTag(Tag.Player))
             {
@@ -89,8 +93,9 @@ namespace TDS.Game.Enemy.Base
 
             Vector3 directionToPlayer = (col.transform.position - transform.position).normalized;
             float distanceToPlayer = (col.transform.position - transform.position).magnitude;
+            Vector3 initialDirection = Quaternion.Euler(0, 0, _initialDirectionAngle) * transform.right;
             if (distanceToPlayer <= _noticeDistance ||
-                (Vector3.Angle(transform.right, directionToPlayer) <= _viewAngle / 2 &&
+                (Vector3.Angle(initialDirection, directionToPlayer) <= _viewAngle / 2 &&
                  distanceToPlayer <= _viewDistance))
             {
                 RaycastHit2D hit =
